b1027045264957da34abc9950b5e045dd0331f69
49EHyeon42 <49ehyeon42@gmail.com>
2024-07-04 11:49:21 +0900

    :recycle: SimpleShortenUrlService 중 코드 일부 리팩터링
    1. shortenUrlRepository 필드 final 선언
    2. @Autowired 생략
    3. if문 중괄호 추가
    4. return 인라인화

--- src/main/java/dev/ehyeon/surl/application/SimpleShortenUrlService.java
+++ src/main/java/dev/ehyeon/surl/application/SimpleShortenUrlService.java
@@ -7,15 +7,13 @@ import dev.ehyeon.surl.domain.ShortenUrlRepository;
 import dev.ehyeon.surl.application.request.ShortenUrlCreateRequest;
 import dev.ehyeon.surl.application.response.ShortenUrlCreateResponse;
 import dev.ehyeon.surl.application.response.ShortenUrlInformationResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleShortenUrlService {
 
-    private ShortenUrlRepository shortenUrlRepository;
+    private final ShortenUrlRepository shortenUrlRepository;
 
-    @Autowired
     SimpleShortenUrlService(ShortenUrlRepository shortenUrlRepository) {
         this.shortenUrlRepository = shortenUrlRepository;
     }
@@ -25,39 +23,38 @@ public class SimpleShortenUrlService {
         String shortenUrlKey = getUniqueShortenUrlKey();
 
         ShortenUrl shortenUrl = new ShortenUrl(originalUrl, shortenUrlKey);
+
         shortenUrlRepository.saveShortenUrl(shortenUrl);
 
-        ShortenUrlCreateResponse shortenUrlCreateResponse = new ShortenUrlCreateResponse(shortenUrl.getOriginalUrl(), shortenUrl.getShortenUrlKey());
-        return shortenUrlCreateResponse;
+        return new ShortenUrlCreateResponse(shortenUrl.getOriginalUrl(), shortenUrl.getShortenUrlKey());
     }
 
     public String getOriginalUrlByShortenUrlKey(String shortenUrlKey) {
         ShortenUrl shortenUrl = shortenUrlRepository.findShortenUrlByShortenUrlKey(shortenUrlKey);
 
-        if (null == shortenUrl)
+        if (null == shortenUrl) {
             throw new NotFoundShortenUrlException();
+        }
 
         shortenUrl.increaseRedirectCount();
+
         shortenUrlRepository.saveShortenUrl(shortenUrl);
 
-        String originalUrl = shortenUrl.getOriginalUrl();
-
-        return originalUrl;
+        return shortenUrl.getOriginalUrl();
     }
 
     public ShortenUrlInformationResponse getShortenUrlInformationByShortenUrlKey(String shortenUrlKey) {
         ShortenUrl shortenUrl = shortenUrlRepository.findShortenUrlByShortenUrlKey(shortenUrlKey);
 
-        if (null == shortenUrl)
+        if (null == shortenUrl) {
             throw new NotFoundShortenUrlException();
+        }
 
-        ShortenUrlInformationResponse shortenUrlInformationResponse = new ShortenUrlInformationResponse(
+        return new ShortenUrlInformationResponse(
                 shortenUrl.getOriginalUrl(),
                 shortenUrl.getShortenUrlKey(),
                 shortenUrl.getRedirectCount()
         );
-
-        return shortenUrlInformationResponse;
     }
 
     private String getUniqueShortenUrlKey() {
@@ -68,8 +65,9 @@ public class SimpleShortenUrlService {
             String shortenUrlKey = ShortenUrl.generateShortenUrlKey();
             ShortenUrl shortenUrl = shortenUrlRepository.findShortenUrlByShortenUrlKey(shortenUrlKey);
 
-            if (null == shortenUrl)
+            if (null == shortenUrl) {
                 return shortenUrlKey;
+            }
         }
 
         throw new LackOfShortenUrlKeyException();
