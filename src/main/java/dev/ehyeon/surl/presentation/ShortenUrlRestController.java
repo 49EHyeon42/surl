287d1ce1b97f8f8441b3f31a8c914f41ffea3ffd
49EHyeon42 <49ehyeon42@gmail.com>
2024-07-04 13:09:00 +0900

    :recycle: ShortenUrlRestController 중 코드 일부 리팩터링
    1. SimpleShortenUrlService 필드 final 선언
    2. @Autowired 생략
    3. @RequestMapping 대신 @GetMapping, @PostMapping으로 변경
    4. 일부 코드 인라인화

--- src/main/java/dev/ehyeon/surl/presentation/ShortenUrlRestController.java
+++ src/main/java/dev/ehyeon/surl/presentation/ShortenUrlRestController.java
@@ -5,7 +5,6 @@ import dev.ehyeon.surl.application.request.ShortenUrlCreateRequest;
 import dev.ehyeon.surl.application.response.ShortenUrlCreateResponse;
 import dev.ehyeon.surl.application.response.ShortenUrlInformationResponse;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,41 +16,35 @@ import java.net.URISyntaxException;
 @RestController
 public class ShortenUrlRestController {
 
-    private SimpleShortenUrlService simpleShortenUrlService;
+    private final SimpleShortenUrlService simpleShortenUrlService;
 
-    @Autowired
     ShortenUrlRestController(SimpleShortenUrlService simpleShortenUrlService) {
         this.simpleShortenUrlService = simpleShortenUrlService;
     }
 
-    @RequestMapping(value = "/shortenUrl", method = RequestMethod.POST)
+    @PostMapping("/shortenUrl")
     public ResponseEntity<ShortenUrlCreateResponse> createShortenUrl(
             @Valid @RequestBody ShortenUrlCreateRequest shortenUrlCreateRequest
     ) {
-        ShortenUrlCreateResponse shortenUrlCreateResponse =
-                simpleShortenUrlService.generateShortenUrl(shortenUrlCreateRequest);
-        return ResponseEntity.ok(shortenUrlCreateResponse);
+        return ResponseEntity.ok(simpleShortenUrlService.generateShortenUrl(shortenUrlCreateRequest));
     }
 
-    @RequestMapping(value = "/{shortenUrlKey}", method = RequestMethod.GET)
+    @GetMapping("/{shortenUrlKey}")
     public ResponseEntity<?> redirectShortenUrl(
             @PathVariable String shortenUrlKey
     ) throws URISyntaxException {
         String originalUrl = simpleShortenUrlService.getOriginalUrlByShortenUrlKey(shortenUrlKey);
 
-        URI redirectUri = new URI(originalUrl);
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(redirectUri);
+        httpHeaders.setLocation(new URI(originalUrl));
 
         return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
     }
 
-    @RequestMapping(value = "/shortenUrl/{shortenUrlKey}", method = RequestMethod.GET)
+    @GetMapping("/shortenUrl/{shortenUrlKey}")
     public ResponseEntity<ShortenUrlInformationResponse> getShortenUrlInformation(
             @PathVariable String shortenUrlKey
     ) {
-        ShortenUrlInformationResponse shortenUrlInformationResponse =
-                simpleShortenUrlService.getShortenUrlInformationByShortenUrlKey(shortenUrlKey);
-        return ResponseEntity.ok(shortenUrlInformationResponse);
+        return ResponseEntity.ok(simpleShortenUrlService.getShortenUrlInformationByShortenUrlKey(shortenUrlKey));
     }
 }
